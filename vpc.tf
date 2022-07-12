6d36be1b0bc48ed9d81102d8cf06d5fb9bdfab0d
Kamaz <kamaz@xsimple.org>
2022-07-12 11:14:55 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,15 @@
-module "vpc" {
-  source  = "terraform-google-modules/network/google//modules/vpc"
+module "network" {
+  source  = "terraform-google-modules/network/google"
   version = "5.1.0"
-
-  project_id   = var.project
+  # insert required variables here
+  
   network_name = "gaurav-network"
-  routing_mode = "GLOBAL"
+  project_id   = var.project
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
 }
